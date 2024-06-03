7ab5f7709dcd7b2b0df96656189e45d6ae049353
Johannes <jluetzenho@stud.hs-heilbronn.de>
2024-06-03 16:54:32 +0200

    Created Project with databank hosted by filess

--- jlu_foodcourt-app/backend/config/database.js
+++ jlu_foodcourt-app/backend/config/database.js
@@ -1,9 +1,9 @@
 //Database connection
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('bg22foodcourt', 'BG22FoodCourtUser', 'FoodCourtPw', {
-    host: "seserver.se.hs-heilbronn.de",
-    port: "3406",
+const db = new Sequelize('jluFoodCourtWebApp_tornwiseto', 'jluFoodCourtWebApp_tornwiseto', 'b62e31993ded7f315285cb1387dd0e750aeb2386', {
+    host: "tlm.h.filess.io",
+    port: "3305",
     dialect: 'mariadb',
     dialectOptions: {
         options: {
